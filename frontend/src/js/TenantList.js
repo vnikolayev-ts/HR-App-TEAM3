e5b8f463462817a83e4bba8d098cd3dbc2c84790
petemount <petemount@t-online.de>
2024-07-04 12:21:03 +0200

    TenantList.js auf ClientApi.js-Datenquelle umgestellt, aber noch nicht startfähig.

--- frontend/src/js/TenantList.js
+++ frontend/src/js/TenantList.js
@@ -1,10 +1,43 @@
-import tenantData from "../data/tenants.json";
+import { getTenats } from "./ClientApi";
+
+
 import React from "react";
+import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
+
 
 import Navbar from './NavBar';
 
 const TenantList = () => {
+  
+  const [tenantData, setTenantData] = useState(null);
+  const [layout, setLayout] = useState("simple");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const isDataFromLocal = true;
+        const data = await getTenats(isDataFromLocal); //Datenaufruf getTenants
+        const apiLayout = data.layout;
+
+        setTenantData(data);
+        setLayout(apiLayout);
+
+      } catch (error) {
+        console.error('Error fetching Tenant data:', error);
+        //Ggf. zusätzl. Fehlerbeh., wie z.B. eine Fehlermeldung möglich.
+        return <p>Loading... Error </p>; // Ladeanzeige 
+      }
+    };
+
+    fetchData(); // Aufruf fetchData für getTenants
+
+  }, [tenantData, layout] ); //Leeres Array als 2. Argument, damit useEffect nur 1 mal durchläuft.  
+
+  if (!tenantData) {
+    return <p>Loading...</p>; // Ladeanzeige
+  }
+
 return (
   <div class={layout}>
     <Navbar/>
